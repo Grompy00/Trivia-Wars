9ec251a7e5f2108f2d4ab397836780b9d179e4cd
Grompy00 <vinasojupa@gmail.com>
2025-08-02 11:07:19 -0400

    Update server.js

--- my-app/back end/server.js
+++ my-app/back end/server.js
@@ -176,7 +176,9 @@ io.on('connection', (socket) => {
   });
 });
 
+
 const PORT = process.env.PORT || 8080;
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
 });
